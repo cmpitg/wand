d349469d858b7a73e9100bb607436a4157014b83
Duong H. Nguyen <cmpitg@gmail.com>
2014-01-13 02:26:29 +0700

    Basic description about Wand

---
+++ wand.el
@@ -0,0 +1,33 @@
+;;; wand.el --- Magic wand for Emacs - Selecting and executing
+
+;; Copyright (C) 2012-2013  Duong H. Nguyen
+
+;; Author: Duong H. Nguyen <cmpitgATgmail>
+;; Keywords: extensions, tools
+;; URL: https://github.com/cmpitg/wand
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Wand is an extension that allows users to select a piece of text and
+;; perform actions based on predefined patterns.  Wand is inspired by Xiki[1]
+;; and Acme editor[2].
+;;
+;; TODO: Screencast
+
+;;; Code:
+
+(provide 'wand)
+;;; wand.el ends here
