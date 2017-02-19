dbe11a0adc0258236fbb08cb40ad99188f04dff3
rubikitch <rubikitch@ruby-lang.org>
2017-02-19 09:26:07 +0900

    string-empty? -> string-empty-p

--- wand.el
+++ wand.el
@@ -390,7 +390,7 @@ E.g.
                    string-to-execute))
          (action (or (wand:get-rule-action string)
                      #'wand:eval-string)))
-    (unless (string-empty? (s-trim string))
+    (unless (string-empty-p (s-trim string))
       (funcall action string))))
 
 (defun wand:execute-current-line ()
